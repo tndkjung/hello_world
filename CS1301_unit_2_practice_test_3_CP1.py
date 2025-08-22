20e8c261f8bddd5ef17953f8087578c39b5a964e
Juliette Sua Jung <tndkjung@gmail.com>
2025-08-22 17:43:57 -0400

    record code from CS1301 Unit 2 Practice Test 3 Coding Problem 1

---
+++ CS1301_unit_2_practice_test_3_CP1.py
@@ -0,0 +1,8 @@
+#round_up_the_change.py
+#This program calculates the total cost after rounding up and donation.
+original_total = "5.45"
+donation = "0.55"
+
+final_total = float(original_total) + float(donation)
+final_total = int(final_total)
+print("Your total is $" + str(final_total) + " and you donated $" + str(donation))
